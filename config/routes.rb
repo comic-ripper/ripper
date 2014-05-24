9a45f36841133cc5d9e62c0f32f568a673cfe890
Eric Nelson <eric@clean-logix.com>
2014-05-23 20:30:10 -0400

    authenticate the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
 
-  resources :comics, only: [:index, :new, :show, :create] do
-    get 'p/:page', action: :index, on: :collection
-  end
-
-  resources :chapters, only: [:show] do
-    get ':id/:page', action: :show, on: :collection
-  end
-
   devise_for :users
   root controller: :comics, action: :index
 
 
   authenticate :user do
+    resources :comics, only: [:index, :new, :show, :create] do
+      get 'p/:page', action: :index, on: :collection
+    end
+
+    resources :chapters, only: [:show] do
+      get ':id/:page', action: :show, on: :collection
+    end
+
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     require 'sidekiq/web'
     mount Sidekiq::Web => '/sidekiq'
