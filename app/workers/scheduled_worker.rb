ece5cd010b40d93f622b39aeb176c5f1c872836f
Eric Nelson <nelseric.alt@gmail.com>
2015-10-28 19:21:51 -0500

    Increase scheduled check count,
    it is run every 10 minutes now instead of 1 min

--- app/workers/scheduled_worker.rb
+++ app/workers/scheduled_worker.rb
@@ -5,8 +5,8 @@ class ScheduledWorker
 
   def perform
     Comic.not_recently_checked.each(&:delay_check)
-    Chapter.unchecked.limit(100).each(&:delay_check)
-    Page.unchecked.limit(100).each(&:delay_check)
+    Chapter.unchecked.limit(2000).each(&:delay_check)
+    Page.unchecked.limit(2000).each(&:delay_check)
     Chapter.checked.unbuilt.limit(100).select(&:complete?).each(&:delay_build)
   end
 end
